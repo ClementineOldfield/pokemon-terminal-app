29cb3357732869dec5a55d136e1b70febb769c03
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-03-05 10:51:59 +1100

    moved damage system to attack method within pokemon class

--- index.rb
+++ index.rb
@@ -4,11 +4,28 @@ class Pokemon
     def initialize (name, type, attack, health)
         @attack = attack 
         @hp = health 
+        @max_hp = health
         @name = name 
         @type = type
     end
 
     #TODO: Create method for attack. Basic functionality - prints: "attacks opponent"
+
+    def attack 
+        damage = rand 0..10
+        if damage >= 8 
+            puts "Critical hit! Opponent took #{damage} damage."
+        elsif damage <= 2 && damage >= 1
+            puts "Not very effective. Opponent took #{damage} damage."
+        elsif damage == 0
+            puts "No effect. Opponent took #{damage} damage."
+        else 
+            puts "Opponent took #{damage} damage."
+        end
+        @hp = @hp - damage
+        puts "#{@hp} / #{@max_hp} HP"
+    end
+
 end
 
 #Declare multiple instances of pokemon class
@@ -21,22 +38,6 @@ magikarp = Pokemon.new("Magikarp", "Water", rand(1..10), 20)
 
 p "You've picked #{magikarp.name}! He's a #{magikarp.type} type."
 p "Base stats: #{magikarp.attack} damage and #{magikarp.hp} HP"
-
-
-max_health = 20
-
-    damage = rand 0..10
-    if damage >= 8 
-        puts "Critical hit! Opponent took #{damage} damage."
-    elsif damage <= 2 && damage >= 1
-        puts "Not very effective. Opponent took #{damage} damage."
-    elsif damage == 0
-        puts "No effect. Opponent took #{damage} damage."
-    else 
-        puts "Opponent took #{damage} damage."
-    end
-    total_health = max_health - damage
-    puts "#{total_health} / #{max_health} HP"
 
 
     
@@ -56,6 +57,7 @@ while playing
         puts "Attack? y/n"
         fight_input = gets.chomp
         if fight_input == "y"
+            pikachu.attack
             puts "attacked"
         elsif fight_input == "n"
             fighting = false
