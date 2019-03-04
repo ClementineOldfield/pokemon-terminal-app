8119942b69226f1e823766d2c7aba651d4116233
shaahilchand <shaahilchand98@gmail.com>
2019-03-05 09:48:11 +1100

    Added first class

--- index.rb
+++ index.rb
@@ -1,8 +1,20 @@
 class Pokemon 
-    def initialize
+    attr_accessor :attack, :hp, :name, :type
+    def initialize (name, type, attack, health)
+        @attack = attack 
+        @hp = health 
+        @name = name 
+        @type = type
     end
-
 end
+
+
+pikachu = Pokemon.new("Pikachu", "Electric", rand(1..10), 20)
+
+p "You've picked #{pikachu.name}! He's an #{pikachu.type} type."
+p "Base stats: #{pikachu.attack} damage and #{pikachu.hp} HP"
+
+
 
 playing = true
 while playing
