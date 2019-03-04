eb40728bf64d9feef19f9eb48ee596e9ff1cf52d
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-03-05 10:19:13 +1100

    added basic comments

--- index.rb
+++ index.rb
@@ -1,3 +1,4 @@
+#Create pokemon class. TODO: move to other file and import here.
 class Pokemon 
     attr_accessor :attack, :hp, :name, :type
     def initialize (name, type, attack, health)
@@ -8,7 +9,7 @@ class Pokemon
     end
 end
 
-
+#Declare multiple instances of pokemon class
 pikachu = Pokemon.new("Pikachu", "Electric", rand(1..10), 20)
 
 p "You've picked #{pikachu.name}! He's an #{pikachu.type} type."
@@ -20,13 +21,14 @@ p "You've picked #{magikarp.name}! He's a #{magikarp.type} type."
 p "Base stats: #{magikarp.attack} damage and #{magikarp.hp} HP"
 
 
-
+#Start game loop
 playing = true
 while playing
     system("clear")
     puts "please choose a pokemon"
     user_choice = gets.chomp
 
+    #Start fighting loop
     fighting = true
     while fighting 
         puts "your pokemon is #{user_choice}"
@@ -37,12 +39,12 @@ while playing
         elsif fight_input == "n"
             fighting = false
         end
-    end
+    end #End fighting loop
 
     puts "would you like to play again? y/n"
     play_input = gets.chomp
 
     if play_input == "n"
         playing = false
-    end
-end
+    end 
+end #End playing loop
