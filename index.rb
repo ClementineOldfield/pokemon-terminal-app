2d5eb3f2f73befb099313f170558da23a46d1bf9
shaahilchand <shaahilchand98@gmail.com>
2019-03-05 10:35:04 +1100

    added randomness of damage values

--- index.rb
+++ index.rb
@@ -20,7 +20,23 @@ p "You've picked #{magikarp.name}! He's a #{magikarp.type} type."
 p "Base stats: #{magikarp.attack} damage and #{magikarp.hp} HP"
 
 
+max_health = 20
 
+    damage = rand 0..10
+    if damage >= 8 
+        puts "Critical hit! Opponent took #{damage} damage."
+    elsif damage <= 2 && damage >= 1
+        puts "Not very effective. Opponent took #{damage} damage."
+    elsif damage == 0
+        puts "No effect. Opponent took #{damage} damage."
+    else 
+        puts "Opponent took #{damage} damage."
+    end
+    total_health = max_health - damage
+    puts "#{total_health} / #{max_health} HP"
+
+
+    
 playing = true
 while playing
 
