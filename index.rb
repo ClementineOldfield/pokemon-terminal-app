2cd7dfaf21fd87cda3df578e4a1362e4991d4c2f
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-03-05 09:34:06 +1100

    loops and conditionals added to allow very basic play

--- index.rb
+++ index.rb
@@ -6,6 +6,26 @@ end
 
 playing = true
 while playing
+    system("clear")
+    puts "please choose a pokemon"
+    user_choice = gets.chomp
 
-    
+    fighting = true
+    while fighting 
+        puts "your pokemon is #{user_choice}"
+        puts "Attack?"
+        fight_input = gets.chomp
+        if fight_input == "y"
+            puts "attacked"
+        elsif fight_input == "n"
+            fighting = false
+        end
+    end
+
+    puts "would you like to play again?"
+    play_input = gets.chomp
+
+    if play_input == "n"
+        playing = false
+    end
 end
