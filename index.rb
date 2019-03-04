9a8468f1fd7920699b81e24977704fb4c8be2b41
shaahilchand <shaahilchand98@gmail.com>
2019-03-05 09:58:10 +1100

    added second class

--- index.rb
+++ index.rb
@@ -14,6 +14,11 @@ pikachu = Pokemon.new("Pikachu", "Electric", rand(1..10), 20)
 p "You've picked #{pikachu.name}! He's an #{pikachu.type} type."
 p "Base stats: #{pikachu.attack} damage and #{pikachu.hp} HP"
 
+magikarp = Pokemon.new("Magikarp", "Water", rand(1..10), 20)
+
+p "You've picked #{magikarp.name}! He's a #{magikarp.type} type."
+p "Base stats: #{magikarp.attack} damage and #{magikarp.hp} HP"
+
 
 
 playing = true
