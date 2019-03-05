c936d119fdd365e059780b6f16473530f06ca5c5
shaahilchand <shaahilchand98@gmail.com>
2019-03-05 11:27:03 +1100

    Added reset health. Added user choice of pokemon between pikachu and magikarp. Refactored instances of pikachu to user in the fighting loop.

--- index.rb
+++ index.rb
@@ -4,47 +4,50 @@ class Pokemon
     def initialize (name, type, attack, health)
         @attack = attack 
         @hp = health 
+        @max_hp = health
         @name = name 
         @type = type
     end
 
-    #TODO: Create method for attack. Basic functionality - prints: "attacks opponent"
+    def attack 
+        damage = rand 0..10
+        if damage >= 8 
+            puts "Critical hit! Opponent took #{damage} damage."
+        elsif damage <= 2 && damage >= 1
+            puts "Not very effective. Opponent took #{damage} damage."
+        elsif damage == 0
+            puts "No effect. Opponent took #{damage} damage."
+        else 
+            puts "Opponent took #{damage} damage."
+        end
+        @hp = @hp - damage
+        puts "#{@hp} / #{@max_hp} HP"
+    end
+
+    def reset_health
+        @hp = @max_hp
+    end
+
 end
 
 #Declare multiple instances of pokemon class
 pikachu = Pokemon.new("Pikachu", "Electric", rand(1..10), 20)
 
-p "You've picked #{pikachu.name}! He's an #{pikachu.type} type."
-p "Base stats: #{pikachu.attack} damage and #{pikachu.hp} HP"
 
 magikarp = Pokemon.new("Magikarp", "Water", rand(1..10), 20)
-
-p "You've picked #{magikarp.name}! He's a #{magikarp.type} type."
-p "Base stats: #{magikarp.attack} damage and #{magikarp.hp} HP"
-
-
-max_health = 20
-
-    damage = rand 0..10
-    if damage >= 8 
-        puts "Critical hit! Opponent took #{damage} damage."
-    elsif damage <= 2 && damage >= 1
-        puts "Not very effective. Opponent took #{damage} damage."
-    elsif damage == 0
-        puts "No effect. Opponent took #{damage} damage."
-    else 
-        puts "Opponent took #{damage} damage."
-    end
-    total_health = max_health - damage
-    puts "#{total_health} / #{max_health} HP"
-
 
     
 playing = true
 while playing
-    system("clear")
     puts "please choose a pokemon"
-    user_choice = gets.chomp
+    user_choice = gets.chomp.capitalize
+    if user_choice == "Pikachu"
+        user = pikachu
+    elsif user_choice == "Magikarp"
+        user = magikarp
+    else 
+        puts "That's not a valid option"
+    end
 
     #TODO: add conditionals to assign the correct pokemon objects to the user.
 
@@ -52,14 +55,20 @@ while playing
     fighting = true
     while fighting 
         #TODO: access the name from the user's pokemon object instead of a static variable.
-        puts "your pokemon is #{user_choice}"
+        puts "your pokemon is #{user.name}"
         puts "Attack? y/n"
         fight_input = gets.chomp
         if fight_input == "y"
-            puts "attacked"
+            user.attack
+            if user.hp <= 0 
+                fighting = false 
+                user.reset_health
+            end 
         elsif fight_input == "n"
             fighting = false
+            user.reset_health
         end
+
     end #End fighting loop
 
     puts "would you like to play again? y/n"
