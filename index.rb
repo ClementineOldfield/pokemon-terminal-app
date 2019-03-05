5603fc863af367689652674de2bb64db5642f708
shaahilchand <shaahilchand98@gmail.com>
2019-03-05 11:56:48 +1100

    Added user choice while loop if input is invalid

--- index.rb
+++ index.rb
@@ -22,6 +22,7 @@ class Pokemon
         end
         @hp = @hp - damage
         puts "#{@hp} / #{@max_hp} HP"
+        
     end
 
     def reset_health
@@ -30,7 +31,7 @@ class Pokemon
 
 end
 
-#Declare multiple instances of pokemon class
+
 pikachu = Pokemon.new("Pikachu", "Electric", rand(1..10), 20)
 
 
@@ -39,23 +40,30 @@ magikarp = Pokemon.new("Magikarp", "Water", rand(1..10), 20)
     
 playing = true
 while playing
-    puts "please choose a pokemon"
-    user_choice = gets.chomp.capitalize
-    if user_choice == "Pikachu"
-        user = pikachu
-    elsif user_choice == "Magikarp"
-        user = magikarp
-    else 
-        puts "That's not a valid option"
-    end
+    choosing = true
+    while choosing
+        puts "please choose a pokemon"
+        user_choice = gets.chomp.capitalize
+        if user_choice == "Pikachu"
+            user = pikachu
+            opponent = magikarp
+            choosing = false 
+        elsif user_choice == "Magikarp"
+            user = magikarp
+            opponent = pikachu
+            choosing = false 
+        else 
+            puts "That's not a valid option"
+        end
+    end 
 
     #TODO: add conditionals to assign the correct pokemon objects to the user.
 
     #Start fighting loop
     fighting = true
     while fighting 
-        #TODO: access the name from the user's pokemon object instead of a static variable.
-        puts "your pokemon is #{user.name}"
+        puts "#{user_choice} I choose you!"
+        puts "your opponent is #{opponent.name}"
         puts "Attack? y/n"
         fight_input = gets.chomp
         if fight_input == "y"
